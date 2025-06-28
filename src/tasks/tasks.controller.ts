c4c2109f257d4f5e9878138f9124815e6fcab916
Richie <20235894@esen.edu.sv>
2025-06-27 19:45:26 -0600

    feat: implement TasksController and TasksService with findAll and create methods

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
-
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { CreateTaskDto } from './dto/create-taks.dto';
+ 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+    constructor(private readonly service: TasksService) { }
+ 
+    @Get()
+    findAll() {
+        return this.service.findAll();
+    }
+ 
+    @Post()
+    create(@Body() dto: CreateTaskDto) {
+        return this.service.create(dto);
+    }
+}
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
-
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Task } from './entities/task.entity';
+import { CreateTaskDto } from './dto/create-taks.dto';
+ 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+    constructor(@InjectRepository(Task) private repo: Repository<Task>) { }
+ 
+    findAll() {
+        return this.repo.find();
+    }
+ 
+    create(data: CreateTaskDto) {
+        const task = this.repo.create(data);
+        return this.repo.save(task);
+    }
+}
